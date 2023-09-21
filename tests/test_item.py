573cf17f32a26c1e4f1e8d530b5a39cd68e4e4d4
Denis <110720280+mor1nch@users.noreply.github.com>
2023-09-21 17:16:53 +0300

    homework done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item(name="Apple", quantity=10, price=15.5)
+item2 = Item(name="Banana", quantity=7, price=40)
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 155
+    assert item2.calculate_total_price() == 280
+
+
+def test_apply_discount():
+    assert item1.calculate_total_price() == 155
+    item1.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.calculate_total_price() == 124
